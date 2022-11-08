47ec93481dac24fbb5b07da26b6aafa5cd81036d
JBriceno <josealbertino4@gmail.com>
2022-11-08 06:28:42 -0500

    Falta imagen derecha, arreglar texto como quote, y creo que aumentar ligeramente el tamaño de las imagenes

--- src/App.vue
+++ src/App.vue
@@ -4,71 +4,80 @@
       <header class="d-flex flex-row justify-content-between m-4">
         <div class="d-flex flex-row">
           <h1 class="fw-bold px-2">Notifications</h1>
-          <button class="number_notification rounded-3">3</button>
+          <button class="number_notification rounded-3">
+            {{ number_notification }}
+          </button>
         </div>
-        
-        <button class="mark">Mark all as read</button>
+
+        <button class="mark" @click.once="readed">Mark all as read</button>
       </header>
 
       <mensaje-notificacion
         :imagen="'https://imgur.com/wXwOouR.jpg'"
-        :name="'Mark Webber'"
-        :text="'reacted to your recent post'"
-        :time="'1m ago'"
-        :actionName="'My first tournament today!'"
+        name="Mark Webber"
+        text="reacted to your recent post"
+        time="1m ago"
+        actionName="My first tournament today!"
+        :new="nuevo"
       ></mensaje-notificacion>
       <mensaje-notificacion
         :imagen="'https://imgur.com/CEyrLJc.jpg'"
-        :name="'Angela Gray'"
-        :text="'followed you'"
-        :time="'5m ago'"
+        name="Angela Gray"
+        text="followed you"
+        time="5m ago"
+        :new="nuevo"
       ></mensaje-notificacion>
       <mensaje-notificacion
-      :imagen="'https://imgur.com/1CuQYMl.jpg'"
-        :name="'Jacob Thompson'"
-        :text="'has joined your group'"
-        :time="'1 day ago'"
-        :actionName="'Chess Club'"
+        :imagen="'https://imgur.com/1CuQYMl.jpg'"
+        name="Jacob Thompson"
+        text="has joined your group"
+        time="1 day ago"
+        actionName="Chess Club"
+        :new="nuevo"
       ></mensaje-notificacion>
-      
+
       <mensaje-notificacion
-      :imagen="'https://imgur.com/VqZ073J.jpg'"
-        :name="'Rizky Hasanuddin'"
-        :text="'sent you a private message'"
-        :time="'5 days ago'"
+        :imagen="'https://imgur.com/VqZ073J.jpg'"
+        name="Rizky Hasanuddin"
+        text="sent you a private message"
+        time="5 days ago"
         :message="messageText"
       ></mensaje-notificacion>
       <mensaje-notificacion
-      
-      :imagen="'https://imgur.com/XwvL6M6.jpg'"
-        :name="'Kimberly Smith'"
-        :text="'commented on your picture'"
-        :time="'1 week ago'"
-        :actionName="'My first tournament today!'"
+        :imagen="'https://imgur.com/XwvL6M6.jpg'"
+        name="Kimberly Smith"
+        text="commented on your picture"
+        time="1 week ago"
         :imagenDerecha="'https://imgur.com/R5RQgwq.jpg'"
       ></mensaje-notificacion>
       <mensaje-notificacion
-      :imagen="'https://imgur.com/5heoqEf.jpg'"
-        :name="'Nathan Peterson'"
-        :text="'reacted to your recent post'"
-        :time="'2 weeks ago'"
-        :actionName="'end-game strategies to increase your win rate'"
+        :imagen="'https://imgur.com/5heoqEf.jpg'"
+        name="Nathan Peterson"
+        text="reacted to your recent post"
+        time="2 weeks ago"
+        actionName="end-game strategies to increase your win rate"
       ></mensaje-notificacion>
       <mensaje-notificacion
-      :imagen="'https://imgur.com/NJjxyzY.jpg'"
-        :name="'Anna Kim'"
-        :text="'left the group'"
-        :time="'2 weeks ago'"
-        :actionName="'Chess Club'"
+        :imagen="'https://imgur.com/NJjxyzY.jpg'"
+        name="Anna Kim"
+        text="left the group"
+        time="2 weeks ago"
+        actionName="Chess Club"
       ></mensaje-notificacion>
     </div>
   </main>
 </template>
 
-<script lang="ts"> 
-let number_notification: number;
-let messageText: string;
-messageText = "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.";
+<script setup lang="ts">
+import { ref } from "vue";
+let number_notification = ref(3);
+let messageText =
+  "Hello, thanks for setting up the Chess Club. I've been a member for a few weeks now and I'm already having lots of fun and improving my game.";
+let nuevo = ref(true);
+const readed = () => {
+  nuevo.value = false;
+  number_notification.value = 0;
+};
 </script>
 
 <style lang="scss">
--- src/components/MensajeNotificacion.vue
+++ src/components/MensajeNotificacion.vue
@@ -1,29 +1,51 @@
 <template>
-<div class="d-flex fondo mx-3 my-2">
-    <img :src="mainProps.imagen" :alt="'imagen de '+ mainProps.name" />
-    <a class="fw-bold nombre">{{mainProps.name}} </a>  <p class="color-text px-2"> {{mainProps.text}}</p> <a class="fw-bold action" v-if="mainProps.actionName">{{mainProps.actionName}}</a>
-    <p><br>{{mainProps.time}}</p>
-    <p v-if="mainProps.message">{{mainProps.message}}</p>
-    <img v-if="mainProps.imagenDerecha" :src="mainProps.imagenDerecha" />
-</div>
+  <section class="d-flex flex-column mx-3 my-2" :class="{ fondo: mainProps.new }">
+    <div class="d-flex">
+      <img
+        :src="mainProps.imagen"
+        :alt="'imagen de' + mainProps.name"
+        class="mx-3 my-auto"
+      />
+      <div class="d-flex mt-3">
+        <p>
+          <a class="fw-bold nombre">{{ mainProps.name }} </a>
+          <span class="color-text px-2"> {{ mainProps.text }}</span>
+          <a class="fw-bold action" v-if="mainProps.actionName">{{
+            mainProps.actionName
+          }}</a>
+          <br /><span class="color-text">{{ mainProps.time }}</span>
+        </p>
+        <img
+          v-if="mainProps.imagenDerecha"
+          :src="mainProps.imagenDerecha"
+          class="my-1"
+        />
+      </div>
+    </div>
+
+    <div v-if="mainProps.message" class="border mensaje w-75">
+      <p v-if="mainProps.message">{{ mainProps.message }}</p>
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
-import {defineProps} from "vue";
+import { defineProps } from "vue";
 const mainProps = defineProps<{
-  imagen: string,
-  name: string,
-  text: string,
-  actionName?: string,
-  time: string,
-  message?: string,
-  imagenDerecha?: string,
+  imagen: string;
+  name: string;
+  text: string;
+  actionName?: string;
+  time: string;
+  message?: string;
+  imagenDerecha?: string;
+  new?: boolean;
 }>();
 </script>
 
 <style scoped lang="scss">
 $blue: hsl(219, 85%, 26%);
-$verylightgrayishblue:hsl(210, 60%, 98%);
+$verylightgrayishblue: hsl(210, 60%, 98%);
 $Grayishblue: hsl(219, 14%, 63%);
 img {
   width: 32px;
@@ -48,6 +70,10 @@ a {
 
 .nombre {
   color: black;
+}
+
+.mensaje {
+  margin-left: 40px;
 }
 
 .action {
